25c227e49d7c9b8c5b381a1e9271a1a14c640a76
Qosdil A <qosdil@yahoo.com>
2024-09-22 01:22:04 +0700

    Complete the response of GetPosts().

--- service.go
+++ service.go
@@ -1,10 +1,20 @@
 package x_clone_post_srv
 
-import "context"
+import (
+	"context"
+	"time"
+)
+
+type User struct {
+	ID       string `json:"id"`
+	Username string `json:"username"`
+}
 
 type Post struct {
-	ID   string `json:"id"`
-	Post string `json:"post"`
+	ID        string `json:"id"`
+	Post      string `json:"post"`
+	CreatedAt int64  `json:"created_at"`
+	User      User   `json:"user"`
 }
 
 type Service interface {
@@ -18,7 +28,17 @@ func NewDbService() Service {
 }
 
 func (s *dbService) GetPosts(ctx context.Context) (posts []Post, err error) {
-	posts = append(posts, Post{ID: "fffa333", Post: "Hello"})
-	posts = append(posts, Post{ID: "fffa334", Post: "Hi! This is my first post."})
+	posts = append(posts, Post{
+		ID: "fffa333", Post: "Hello", CreatedAt: time.Now().Unix(),
+		User: User{
+			ID:       "fjjlp341k",
+			Username: "mary",
+		},
+	})
+	posts = append(posts, Post{ID: "fffa334", Post: "Hi! This is my first post.", CreatedAt: time.Now().Unix(),
+		User: User{
+			ID:       "j99fjjjf",
+			Username: "alan388",
+		}})
 	return posts, nil
 }
